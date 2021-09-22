1c8a9a4951fd3ee018588f979ce427c45831c7bd
이름 <ktg6361@gmail.com>
2021-09-22 16:56:01 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -14,6 +14,11 @@ function login() {
 
   fetch("/login", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(req),
-  });
+  })
+  .then((res) => res.json())
+  .then((res) => console.log(res));
 };
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,43 @@
 "use strict";
 
-const hello = (req, res) => {
-  res.render("home/index");
+
+const output = {
+  hello: (req, res) => {
+    res.render("home/index");
+  },  
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["woori", "taegyu", "wecode"],
+  psword: ["123", "1234", "123456"],
 };
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      psword = req.body.psword;
+
+    if(users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.psword[idx] === psword) {
+        return res.json({
+          succees: true,
+        });
+      }
+    }
+
+    return res.json({
+      succees: false,
+      msg: "로그인에 실패하셨습니다.",
+    })
+  },
+};
+
 
 module.exports = {
-  hello,
-  login,
+  output,
+  process,
 };
